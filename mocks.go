f13e3c5a97e981513151073903e6615e86e2b42a
Vinícius Garcia <vingarcia00@gmail.com>
2025-08-22 15:31:36 -0300

    Improve the mock interface

--- mocks.go
+++ mocks.go
@@ -4,44 +4,50 @@ import "context"
 
 // Mock mocks the Provider interface
 type Mock struct {
-	DebugFn func(ctx context.Context, title string, valueMaps ...Body)
-	InfoFn  func(ctx context.Context, title string, valueMaps ...Body)
-	WarnFn  func(ctx context.Context, title string, valueMaps ...Body)
-	ErrorFn func(ctx context.Context, title string, valueMaps ...Body)
-	FatalFn func(ctx context.Context, title string, valueMaps ...Body)
+	DebugFn func(ctx context.Context, title string, body Body)
+	InfoFn  func(ctx context.Context, title string, body Body)
+	WarnFn  func(ctx context.Context, title string, body Body)
+	ErrorFn func(ctx context.Context, title string, body Body)
+	FatalFn func(ctx context.Context, title string, body Body)
 }
 
 // Debug implements the klog.Provider interface
 func (m Mock) Debug(ctx context.Context, title string, valueMaps ...Body) {
 	if m.DebugFn != nil {
-		m.DebugFn(ctx, title, valueMaps...)
+		m.DebugFn(ctx, title, mergeMaps(valueMaps))
 	}
 }
 
 // Info implements the klog.Provider interface
 func (m Mock) Info(ctx context.Context, title string, valueMaps ...Body) {
 	if m.InfoFn != nil {
-		m.InfoFn(ctx, title, valueMaps...)
+		m.InfoFn(ctx, title, mergeMaps(valueMaps))
 	}
 }
 
 // Warn implements the klog.Provider interface
 func (m Mock) Warn(ctx context.Context, title string, valueMaps ...Body) {
 	if m.WarnFn != nil {
-		m.WarnFn(ctx, title, valueMaps...)
+		m.WarnFn(ctx, title, mergeMaps(valueMaps))
 	}
 }
 
 // Error implements the klog.Provider interface
 func (m Mock) Error(ctx context.Context, title string, valueMaps ...Body) {
 	if m.ErrorFn != nil {
-		m.ErrorFn(ctx, title, valueMaps...)
+		m.ErrorFn(ctx, title, mergeMaps(valueMaps))
 	}
 }
 
 // Fatal implements the klog.Provider interface
 func (m Mock) Fatal(ctx context.Context, title string, valueMaps ...Body) {
 	if m.FatalFn != nil {
-		m.FatalFn(ctx, title, valueMaps...)
+		m.FatalFn(ctx, title, mergeMaps(valueMaps))
 	}
+}
+
+func mergeMaps(sources []Body) Body {
+	body := Body{}
+	MergeMaps(&body, sources...)
+	return body
 }
